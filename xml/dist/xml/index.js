3328046cb2e9a1cf8e66b2ef2aae444664b0dc20
Crol55 <carlosorantes77@gmail.com>
2021-06-13 12:40:27 -0600

    Cambios a package.json para ejecutar con un comando la construccion de la gramatica descendente

--- xml/dist/xml/index.js
+++ xml/dist/xml/index.js
@@ -2,12 +2,35 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 //import {Atributo } from '../CLASES/Atributo';
 const analizador = require('./GRAMATICAS/Gramatica1');
+const analizadorDesc = require('./GRAMATICAS/gdesc');
 console.log("Esta funcionando todo");
 //const ambito:Ambito = new Ambito(null);
 let hash; // Tabla hash, que representa la tabla de simbolos interna del ambito
 hash = {};
 hash['hola'] = 'hola';
-const elementos = analizador.parse(`
+const elementos = analizadorDesc.parse(`
+<?xml version="1.0" encoding="UTF-8"?>
+<bookstore libreria="Usac" ciudad="Guatemala">
+  <book category="children">
+          hola    &amp; mundo
+    <title>   !ABC ABC</title>
+    <author>!2013 = "abc_123"
+    continuacion </author>
+    <year>2005</year>
+    <price>
+        29.99
+        <WORK>ABC</WORK>
+    </price>
+  </book>
+  <book2 category="web &amp;">
+    <title2>Learning   XML</title2>
+    <author2>Erik     T. Ray = ""?</author2>
+    <year2>2003</year2>
+    <price2>39.95</price2>
+  </book2>
+</bookstore>
+`);
+/*const elementos = analizador.parse(`
 <?xml version="1.0" encoding="UTF-8"?>
 <bookstore libreria="Usac" ciudad="Guatemala">
   <book category="children">
@@ -30,6 +53,7 @@ const elementos = analizador.parse(`
 </bookstore>
 `);
 //console.log(typeof elementos);
-let elementosCast = elementos;
-const ambitoGlobal = elementosCast.construirTablaSimbolos(null);
-console.log(ambitoGlobal);
+let elementoRaiz:Elemento = <Elemento>elementos;
+
+const ambitoGlobal:Ambito = elementoRaiz.construirTablaSimbolos(null); // construirTablaSimbolos es funcion recursiva
+console.log(ambitoGlobal);*/
--- xml/index.ts
+++ xml/index.ts
@@ -2,6 +2,7 @@
 
 //import {Atributo } from '../CLASES/Atributo';
 const analizador = require('./GRAMATICAS/Gramatica1');
+const analizadorDesc = require('./GRAMATICAS/gdesc');
 import { Ambito} from '../CLASES/Ambito';
 import { Elemento } from '../CLASES/Elemento';
 
@@ -11,8 +12,29 @@ let hash:{ [id:string] : string }; // Tabla hash, que representa la tabla de sim
 hash = {};
 hash['hola'] = 'hola';
 
-
-const elementos = analizador.parse(`
+const elementos = analizadorDesc.parse(`
+<?xml version="1.0" encoding="UTF-8"?>
+<bookstore libreria="Usac" ciudad="Guatemala">
+  <book category="children">
+          hola    &amp; mundo
+    <title>   !ABC ABC</title>
+    <author>!2013 = "abc_123"
+    continuacion </author>
+    <year>2005</year>
+    <price>
+        29.99
+        <WORK>ABC</WORK>
+    </price>
+  </book>
+  <book2 category="web &amp;">
+    <title2>Learning   XML</title2>
+    <author2>Erik     T. Ray = ""?</author2>
+    <year2>2003</year2>
+    <price2>39.95</price2>
+  </book2>
+</bookstore>
+`);
+/*const elementos = analizador.parse(`
 <?xml version="1.0" encoding="UTF-8"?>
 <bookstore libreria="Usac" ciudad="Guatemala">
   <book category="children">
@@ -38,4 +60,4 @@ const elementos = analizador.parse(`
 let elementoRaiz:Elemento = <Elemento>elementos;
 
 const ambitoGlobal:Ambito = elementoRaiz.construirTablaSimbolos(null); // construirTablaSimbolos es funcion recursiva
-console.log(ambitoGlobal);
+console.log(ambitoGlobal);*/
