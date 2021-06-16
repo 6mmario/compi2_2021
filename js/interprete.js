e332f246687b3d53838ac49a34c81a841b644a91
Kvn1004 <pex089@gmail.com>
2021-06-16 17:04:23 -0600

    Reporte de errores funcional

--- js/interprete.js
+++ js/interprete.js
@@ -1,26 +1,48 @@
+var lista_errores = [];
+
 function ejecutarCodigo() {
     var entradaXML = getXML();
     var entradaQuery = getXPath();
     var xml = new Elemento(null);    
-    xml = Gramatica1.parse(entradaXML);
-    //console.log(xml);
-    var query = gramatica.parse(entradaQuery);
-    //console.log(query);
-    query.forEach(consulta => {
-        consulta.forEach(nodo => {
-            //console.log(nodo);
-        })
+    try{
+        xml = Gramatica1.parse(entradaXML);
+        var query = gramatica.parse(entradaQuery);
+        const ambitoGlobal = xml.construirTablaSimbolos(null); 
+        query.forEach(consulta => {
+            consulta.forEach(nodo => {
+                console.log(nodo.identificador)
+            })
+        });
+    }catch(e){
+        console.log(e);
+    }
+    if (lista_errores.length > 0){
+        crearReporteErrores();
+    }
+}
+
+function crearReporteErrores(){
+    var reporte = "<!DOCTYPE html>\n\
+    <html lang=\"en\">\n\n\
+    <head>\n\
+      <title>XPath - Reporte de erroes</title>\n\
+    <table class=\"table table-striped\">\n\
+    <thead>\n\
+      <tr>\n\
+        <th>No.</th>\n\
+        <th>Tipo</th>\n\
+        <th>Descripción</th>\n\
+        <th>Fila</th>\n\
+        <th>Columna</th>\n\
+      </tr>\n\
+    </thead>";
+    var i = 1;
+    lista_errores.forEach(error => {
+        reporte += "<tr>\
+        <th>" + i + "</th><th>" + error.tipo + "</th><th>" + error.descripcion + "</th><th>" + error.fila + "</th><th>" + error.columna + "</th>" ;
+        i = i + 1;
     });
-    getSubXML(xml);
-    //xml.construirTablaSimbolos(ambitoGlobal);
-    
+    reporte+="</table></html>";    
+    var blob = new Blob([reporte], { type: "html/plain;charset=utf-8" });
+    saveAs(blob, "ReporteErrores.html");
 }
-
-function getSubXML(xml){
-    console.log(xml);
-    xml.lista_elementos.forEach(elemento => {
-        console.log(elemento.identificador);
-        getSubXML(xml.lista_elementos);
-    })
-}
-
