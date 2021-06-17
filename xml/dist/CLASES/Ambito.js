f569597e9928347dbff086e32fca04391bead1b8
Crol55 <carlosorantes77@gmail.com>
2021-06-16 18:14:26 -0600

    Se elimino el bundle.js, y modificaciones del dist, sin embargo modificar el dist del xml, no afecta en nada

--- xml/dist/CLASES/Ambito.js
+++ xml/dist/CLASES/Ambito.js
@@ -1,17 +1,16 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.Ambito = void 0;
 /* El ambito internamente almacenara una tabla de simbolos
     La tabla de simbolos podra contener cualquier tipo especificado en 'Tipo.ts'
 */
-var Ambito = /** @class */ (function () {
-    function Ambito(ambito_anterior) {
+class Ambito {
+    constructor(ambito_anterior) {
         this.ambito_anterior = ambito_anterior;
         this.tablaSimbolos = {};
     }
-    Ambito.prototype.agregar = function (id, simbolo) {
+    agregar(id, simbolo) {
         this.tablaSimbolos[id] = simbolo; // insercion a la tabla hash.
-    };
-    return Ambito;
-}());
+    }
+}
 exports.Ambito = Ambito;
--- xml/dist/CLASES/nodo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Nodo = /** @class */ (function () {
-    function Nodo(identificador, dslash) {
-        this.identificador = identificador;
-        this.dslash = dslash;
-    }
-    return Nodo;
-}());
